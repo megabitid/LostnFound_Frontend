64945d4f86202b64974a657d994a055e68423866
billgufran <billgufran@gmail.com>
2020-11-10 17:05:49 +0800

    Update dashboard page with statistic-card

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,6 +1,7 @@
+import { Col, Row, Typography } from 'antd';
 import React from "react";
 import Sidebar from "../../components/sidebar";
-import {Typography} from 'antd'
+import StatCrad from "../../components/statistic-card";
 const { Title } = Typography;
 
 function index(props) {
@@ -9,7 +10,43 @@ function index(props) {
       <Sidebar
         content={
           <div>
-              <Title>Dashboard</Title>
+            <Title>Dashboard</Title>
+            <Row gutter={ [66, 48] }>
+              <Col span={ 6 }>
+                <StatCrad
+                  isIncreased
+                  stats={23}
+                  title="Laporan barang hilang"
+                  change={9}
+                />
+              </Col>
+              <Col span={ 6 }>
+                <StatCrad
+                  stats={11}
+                  title="Barang ditemukan"
+                  change={20}
+                />
+              </Col>
+              <Col span={ 6 }>
+                <StatCrad
+                  stats={98}
+                  title="Barang berhasil diklaim"
+                  change={17}
+                />
+              </Col>
+              <Col span={ 6 }>
+                <StatCrad
+                  isIncreased
+                  stats={16}
+                  title="Barang didonasikan"
+                  change={9}
+                />
+              </Col>
+            </Row>
+            <Row gutter={ 66 }>
+              <Col span={ 12 } />
+              <Col span={ 12 } />
+            </Row>
           </div>
         }
       />
