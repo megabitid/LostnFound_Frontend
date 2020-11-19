759a3015210e37fffc1c302ce46c47550b152b12
fitrianinasir <fitrianinasir8@gmail.com>
2020-11-19 14:52:27 +0800

    input & detail modal

--- src/components/data-table/index.jsx
+++ src/components/data-table/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import swal from "sweetalert";
 import deleteIcon from "../../assets/deleteIcon.png";
 import {
@@ -6,18 +6,14 @@ import {
   EllipsisOutlined,
   FileSearchOutlined,
   SearchOutlined,
-  PlusOutlined,
 } from "@ant-design/icons";
 import {
-  Form,
-  Modal,
   Button,
   DatePicker,
   Input,
   Popover,
   Select,
   Space,
-  Upload,
   Table,
   Typography,
 } from "antd";
@@ -25,56 +21,13 @@ import {
 const { Text } = Typography;
 const { Option } = Select;
 
-// function for show/preview image uploaded
-function getBase64(file) {
-  return new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = () => resolve(reader.result);
-    reader.onerror = (error) => reject(error);
-  });
-}
-
 export default function Index(props) {
-  const [form] = Form.useForm();
-  const [showModal, setShowModal] = useState(false);
-  const [previewVisible, setPreviewVisible] = useState(false);
-  const [previewImg, setPreviewImg] = useState("");
-  const [previewTitle, setPreviewTitle] = useState("");
-  const [fileList, setFileList] = useState([
-    {
-      uid: "-1",
-      name: "image.png",
-      status: "done",
-      url:
-        "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
-    },
-    {
-      uid: "-2",
-      name: "image.png",
-      status: "done",
-      url:
-        "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
-    },
-    {
-      uid: "-3",
-      name: "image.png",
-      status: "done",
-      url:
-        "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
-    },
-  ]);
-
+  
   // -- table content start --
 
   // show particular photo from table
   const showPhoto = () => {
     alert("Showing Photo");
-  };
-
-  // show data detail from table
-  const dataDetails = () => {
-    alert("Detail");
   };
 
   // delete function of table
@@ -103,7 +56,7 @@ export default function Index(props) {
   // action section of table
   const content = (
     <Space direction="vertical">
-      <Button type="text" icon={<FileSearchOutlined />} onClick={dataDetails}>
+      <Button type="text" icon={<FileSearchOutlined />} onClick={() => props.detailModal(true)}>
         Detail
       </Button>
       <Button type="text" icon={<DeleteOutlined />} onClick={deleteData}>
@@ -179,34 +132,6 @@ export default function Index(props) {
 
   // -- table content end --
 
-  // -- input modal content start --
-  const handlePreview = async (file) => {
-    if (!file.url && !file.preview) {
-      file.preview = await getBase64(file.originFileObj);
-    }
-
-    setPreviewImg(file.url || file.preview);
-    setPreviewVisible(true);
-    setPreviewTitle(
-      file.name || file.url.substring(file.url.lastIndexOf("/") + 1)
-    );
-  };
-
-  const handleChange = ({ fileList }) => setFileList(fileList);
-
-  const uploadButton = (
-    <div>
-      <PlusOutlined />
-      <div style={{ marginTop: 8 }}>Upload</div>
-    </div>
-  );
-
-  // input modal content end
-
-  function submitForm() {
-    alert("form submitted");
-  }
-
   return (
     <div>
       <Space
@@ -229,99 +154,17 @@ export default function Index(props) {
             <Option value="electronic">Elektronik</Option>
           </Select>
         </Space>
-        <Button type="primary" size="large" onClick={() => setShowModal(true)}>
+        <Button
+          type="primary"
+          size="large"
+          onClick={() => {
+            props.inputModal(true);
+          }}
+        >
           Input data
         </Button>
       </Space>
-      <Table columns={columns} dataSource={props.dataWithIndex}/>
-
-      {/* INPUT MODAL */}
-      <Modal
-        visible={showModal}
-        title="Input Data Barang"
-        style={{ top: 20 }}
-        onCancel={() => setShowModal(false)}
-        footer={[
-          <Button key="back" onClick={() => setShowModal(false)}>
-            Cancel
-          </Button>,
-          <Button key="submit" type="primary" loading="" onClick={submitForm}>
-            Submit
-          </Button>,
-        ]}
-      >
-        <Form form={form} layout="vertical">
-          <Form.Item label="Nama Barang">
-            <Input placeholder="Nama Barang" />
-          </Form.Item>
-          <Form.Item label="Tanggal Kehilangan">
-            <DatePicker
-              size="default"
-              placeholder="Tanggal Kehilangan"
-              style={{ width: "100%" }}
-            />
-          </Form.Item>
-
-          <Form.Item label="Lokasi Kehilangan">
-            <Select
-              size="default"
-              placeholder="Lokasi Kehilangan"
-              style={{ width: "100%" }}
-            >
-              <Option value="bag-wallet">Stasiun A</Option>
-              <Option value="other">Stasiun B</Option>
-              <Option value="electronic">Stasiun C</Option>
-            </Select>
-          </Form.Item>
-          <Form.Item label="Kategori Barang">
-            <Select
-              size="default"
-              placeholder="Kategori Barang"
-              style={{ width: "100%" }}
-            >
-              <Option value="bag-wallet">Kategori A</Option>
-              <Option value="other">Kategori B</Option>
-              <Option value="electronic">Kategori C</Option>
-            </Select>
-          </Form.Item>
-          <Form.Item label="Stasus">
-            <Select
-              size="default"
-              placeholder="Stasus"
-              style={{ width: "100%" }}
-            >
-              <Option value="bag-wallet">Status A</Option>
-              <Option value="other">Status B</Option>
-              <Option value="electronic">Status C</Option>
-            </Select>
-          </Form.Item>
-          <Form.Item label="Deskripsi">
-            <Input.TextArea placeholder="Deskripsi" />
-          </Form.Item>
-          <span className="optional-text">*Optional</span>
-          <Form.Item label="Foto">
-            <Upload
-              action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-              listType="picture-card"
-              fileList={fileList}
-              onPreview={handlePreview}
-              onChange={handleChange}
-            >
-              {fileList.length >= 8 ? null : uploadButton}
-            </Upload>
-            <Modal
-              visible={previewVisible}
-              title={previewTitle}
-              footer={null}
-              onCancel={() => setPreviewVisible(false)}
-            >
-              <img alt="example" style={{ width: "100%" }} src={previewImg} />
-            </Modal>
-          </Form.Item>
-        </Form>
-      </Modal>
-
-      
+      <Table columns={columns} dataSource={props.dataWithIndex} />
     </div>
   );
 }
--- src/pages/found/index.jsx
+++ src/pages/found/index.jsx
@@ -1,46 +1,62 @@
-import { Typography } from "antd";
-import React from "react";
+import React, { useState } from "react";
+import {
+  Modal,
+  Button,
+  Form,
+  Input,
+  DatePicker,
+  Select,
+  Upload,
+  Typography,
+} from "antd";
+import { PlusOutlined } from "@ant-design/icons";
 import DataTable from "../../components/data-table";
 import Sidebar from "../../components/sidebar";
-import swal from "sweetalert";
-import deleteIcon from "../../assets/deleteIcon.png";
+
 const { Title } = Typography;
+const { Option } = Select;
 
-function index(props) {
+function getBase64(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = (error) => reject(error);
+  });
+}
 
-  const showPhoto = () => {
-    alert("Showing Photo");
-  };
+function Index(props) {
+  const [form] = Form.useForm();
+  const [showInputModal, setShowInputModal] = useState(false);
+  const [showDetailModal, setShowDetailModal] = useState(false)
+  const [previewVisible, setPreviewVisible] = useState(false);
+  const [previewImg, setPreviewImg] = useState("");
+  const [previewTitle, setPreviewTitle] = useState("");
 
-  const dataDetails = () => {
-    alert("Detail");
-  };
-
-  const deleteData = () => {
-    swal({
-      className: "alert-delete",
-      icon: deleteIcon,
-      title: "Hapus data dari tabel ?",
-      text: "Setelah dihapus, data tersebut tidak akan muncul di dalam tabel",
-      dangerMode: true,
-      buttons: {
-        cancel: {
-          text: "Batal",
-          visible: true,
-          className: "cancel-button",
-        },
-        confirm: {
-          text: "Hapus",
-          value: true,
-          visible: true,
-        },
-      },
-    });
-  };
-
-  function test(){
-    console.log('tes')
-  }
+  // -- table data start --
+  const [fileList, setFileList] = useState([
+    {
+      uid: "-1",
+      name: "image.png",
+      status: "done",
+      url:
+        "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
+    },
+    {
+      uid: "-2",
+      name: "image.png",
+      status: "done",
+      url:
+        "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
+    },
+    {
+      uid: "-3",
+      name: "image.png",
+      status: "done",
+      url:
+        "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
+    },
+  ]);
 
   const data = [
     {
@@ -96,14 +112,244 @@ function index(props) {
   ];
 
   const dataWithIndex = data.map((el, index) => ({ no: index + 1, ...el }));
-  console.log(props.isShowModal)
+
+  // -- table data end --
+
+  // -- input modal content start --
+
+  const inputModal = (isShow) => {
+    setShowInputModal(isShow);
+  };
+
+  const handlePreview = async (file) => {
+    if (!file.url && !file.preview) {
+      file.preview = await getBase64(file.originFileObj);
+    }
+
+    setPreviewImg(file.url || file.preview);
+    setPreviewVisible(true);
+    setPreviewTitle(
+      file.name || file.url.substring(file.url.lastIndexOf("/") + 1)
+    );
+  };
+
+  const handleChange = ({ fileList }) => setFileList(fileList);
+
+  const uploadButton = (
+    <div>
+      <PlusOutlined />
+      <div style={{ marginTop: 8 }}>Upload</div>
+    </div>
+  );
+
+  function submitForm() {
+    alert("form submitted");
+  }
+  // -- input modal content end --
+
+
+  // -- detail modal start
+
+  const detailModal = (isShow) => {
+    setShowDetailModal(isShow);
+  };
   return (
     <div>
       <Sidebar
         content={
           <div>
             <Title>Barang Ditemukan</Title>
-            <DataTable dataWithIndex={dataWithIndex} />
+            <DataTable dataWithIndex={dataWithIndex} inputModal={inputModal} detailModal={detailModal} />
+
+            {/* INPUT MODAL */}
+            <Modal
+              visible={showInputModal}
+              title="Input Data Barang"
+              style={{ top: 20 }}
+              onCancel={() => setShowInputModal(false)}
+              footer={[
+                <Button key="back" onClick={() => setShowInputModal(false)}>
+                  Cancel
+                </Button>,
+                <Button
+                  key="submit"
+                  type="primary"
+                  loading=""
+                  onClick={submitForm}
+                >
+                  Submit
+                </Button>,
+              ]}
+            >
+              <Form form={form} layout="vertical">
+                <Form.Item label="Nama Barang">
+                  <Input placeholder="Nama Barang" />
+                </Form.Item>
+                <Form.Item label="Tanggal Kehilangan">
+                  <DatePicker
+                    size="default"
+                    placeholder="Tanggal Kehilangan"
+                    style={{ width: "100%" }}
+                  />
+                </Form.Item>
+
+                <Form.Item label="Lokasi Kehilangan">
+                  <Select
+                    size="default"
+                    placeholder="Lokasi Kehilangan"
+                    style={{ width: "100%" }}
+                  >
+                    <Option value="bag-wallet">Stasiun A</Option>
+                    <Option value="other">Stasiun B</Option>
+                    <Option value="electronic">Stasiun C</Option>
+                  </Select>
+                </Form.Item>
+                <Form.Item label="Kategori Barang">
+                  <Select
+                    size="default"
+                    placeholder="Kategori Barang"
+                    style={{ width: "100%" }}
+                  >
+                    <Option value="bag-wallet">Kategori A</Option>
+                    <Option value="other">Kategori B</Option>
+                    <Option value="electronic">Kategori C</Option>
+                  </Select>
+                </Form.Item>
+                <Form.Item label="Stasus">
+                  <Select
+                    size="default"
+                    placeholder="Stasus"
+                    style={{ width: "100%" }}
+                  >
+                    <Option value="bag-wallet">Status A</Option>
+                    <Option value="other">Status B</Option>
+                    <Option value="electronic">Status C</Option>
+                  </Select>
+                </Form.Item>
+                <Form.Item label="Deskripsi">
+                  <Input.TextArea placeholder="Deskripsi" />
+                </Form.Item>
+                <span className="optional-text">*Optional</span>
+                <Form.Item label="Foto">
+                  <Upload
+                    action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                    listType="picture-card"
+                    fileList={fileList}
+                    onPreview={handlePreview}
+                    onChange={handleChange}
+                  >
+                    {fileList.length >= 8 ? null : uploadButton}
+                  </Upload>
+                  <Modal
+                    visible={previewVisible}
+                    title={previewTitle}
+                    footer={null}
+                    onCancel={() => setPreviewVisible(false)}
+                  >
+                    <img
+                      alt="example"
+                      style={{ width: "100%" }}
+                      src={previewImg}
+                    />
+                  </Modal>
+                </Form.Item>
+              </Form>
+            </Modal>
+
+            {/* DETAIL/UPDATE MODAL */}
+            <Modal
+              visible={showDetailModal}
+              title="Detail Data Barang"
+              style={{ top: 20 }}
+              onCancel={() => setShowDetailModal(false)}
+              footer={[
+                <Button key="back" onClick={() => setShowDetailModal(false)}>
+                  Cancel
+                </Button>,
+                <Button
+                  key="submit"
+                  type="primary"
+                  loading=""
+                  onClick={submitForm}
+                >
+                  Submit
+                </Button>,
+              ]}
+            >
+              <Form form={form} layout="vertical">
+                <Form.Item label="Nama Barang">
+                  <Input placeholder="Nama Barang" />
+                </Form.Item>
+                <Form.Item label="Tanggal Kehilangan">
+                  <DatePicker
+                    size="default"
+                    placeholder="Tanggal Kehilangan"
+                    style={{ width: "100%" }}
+                  />
+                </Form.Item>
+
+                <Form.Item label="Lokasi Kehilangan">
+                  <Select
+                    size="default"
+                    placeholder="Lokasi Kehilangan"
+                    style={{ width: "100%" }}
+                  >
+                    <Option value="bag-wallet">Stasiun A</Option>
+                    <Option value="other">Stasiun B</Option>
+                    <Option value="electronic">Stasiun C</Option>
+                  </Select>
+                </Form.Item>
+                <Form.Item label="Kategori Barang">
+                  <Select
+                    size="default"
+                    placeholder="Kategori Barang"
+                    style={{ width: "100%" }}
+                  >
+                    <Option value="bag-wallet">Kategori A</Option>
+                    <Option value="other">Kategori B</Option>
+                    <Option value="electronic">Kategori C</Option>
+                  </Select>
+                </Form.Item>
+                <Form.Item label="Stasus">
+                  <Select
+                    size="default"
+                    placeholder="Stasus"
+                    style={{ width: "100%" }}
+                  >
+                    <Option value="bag-wallet">Status A</Option>
+                    <Option value="other">Status B</Option>
+                    <Option value="electronic">Status C</Option>
+                  </Select>
+                </Form.Item>
+                <Form.Item label="Deskripsi">
+                  <Input.TextArea placeholder="Deskripsi" />
+                </Form.Item>
+                <span className="optional-text">*Optional</span>
+                <Form.Item label="Foto">
+                  <Upload
+                    action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                    listType="picture-card"
+                    fileList={fileList}
+                    onPreview={handlePreview}
+                    onChange={handleChange}
+                  >
+                    {fileList.length >= 8 ? null : uploadButton}
+                  </Upload>
+                  <Modal
+                    visible={previewVisible}
+                    title={previewTitle}
+                    footer={null}
+                    onCancel={() => setPreviewVisible(false)}
+                  >
+                    <img
+                      alt="example"
+                      style={{ width: "100%" }}
+                      src={previewImg}
+                    />
+                  </Modal>
+                </Form.Item>
+              </Form>
+            </Modal>
           </div>
         }
       />
@@ -111,4 +357,4 @@ function index(props) {
   );
 }
 
-export default index;
+export default Index;
