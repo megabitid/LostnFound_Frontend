19848df18dc028fddc767e6cde9b34241cc54a70
billgufran <billgufran@gmail.com>
2020-12-09 17:32:12 +0800

    FIx nama_barang

--- src/components/claim-table/index.jsx
+++ src/components/claim-table/index.jsx
@@ -1,20 +1,20 @@
 import {
-  CheckCircleOutlined,
-  DeleteOutlined,
-  EllipsisOutlined,
+   CheckCircleOutlined,
+   DeleteOutlined,
+   EllipsisOutlined,
 
-  SearchOutlined
+   SearchOutlined
 } from "@ant-design/icons";
 import {
-  Button,
+   Button,
 
-  Input,
-  notification,
-  Popover,
-  Select,
-  Space,
-  Table,
-  Typography
+   Input,
+   notification,
+   Popover,
+   Select,
+   Space,
+   Table,
+   Typography
 } from "antd";
 import Modal from "antd/lib/modal/Modal";
 import axios from "axios";
@@ -135,9 +135,9 @@ export default function Index(props) {
     },
     {
       title: "Nama barang",
-      dataIndex: "barang_id",
-      key: "barang_id",
-      render: (text) => getItem(text)
+      dataIndex: "barang",
+      key: "barang",
+      render: (text) => text.nama_barang,
     },
     {
       title: "Nomor telepon",
